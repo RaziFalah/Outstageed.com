8160b0a0ff7fad00d88507e0ee5434690db2ad09
Razi Falah <92949627+RaziFalah@users.noreply.github.com>
2022-02-11 21:48:25 +0200

    Add files via upload

--- apis/post_api.php
+++ apis/post_api.php
@@ -4,16 +4,17 @@ include "auth_api.php";
 if(isset($_POST['submit'])) {
 
     $title =   $_POST['title'];
-    $desc = $_POST['desc'];
+    $desc = $_POST['Description'];;
     $cat = $_POST['option'];
     $content = $_POST['content'];
+    mysqli_real_escape_string($con, $content);
     $keyword = $_POST['option'];
     $username = $_SESSION["username"];
     $timed = date("Y/m/d");
 
-    $sql = "INSERT INTO article (title, descr, keyword, category, content, reason, timed, username) VALUES ('$title', '$desc', '$keyword', '$cat', '$content', '0', '$timed', '$username')";
+    $sql = "INSERT INTO article (title, descr, keyword, category, content, reason, timed, username) VALUES ('$title', '$desc', '$keyword', '$cat', '$content', '$timed', '$timed', '$username')";
 if(mysqli_query($con, $sql)){
-    echo'<meta http-equiv="refresh" content="time; URL=../app" />';
+    header('Location: ../app');
 } else{
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
 }
