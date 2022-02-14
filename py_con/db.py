c6ca3d40b4dd4b2069ebc9e01225243cc8f5038e
Razi Falah <92949627+RaziFalah@users.noreply.github.com>
2022-02-14 20:46:05 +0200

    Add files via upload

---
+++ py_con/db.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="system"
+)
+
+mycursor = mydb.cursor()
+id = 5
+mycursor.execute(f"SELECT username FROM users Where id={id}")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
